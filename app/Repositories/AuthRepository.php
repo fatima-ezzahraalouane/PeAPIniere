48851f72f6886d1bead25aa8a4d73afd4f50572b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-21 14:59:09 +0000

    PEAP-14 creer les methodes pour register et login et me

---
+++ app/Repositories/AuthRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Repositories\Interfaces\AuthRepositoryInterface;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthRepository implements AuthRepositoryInterface
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'     => 'required|string',
+            'email'    => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+            'role'     => 'in:client,employee,admin'
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'role'     => $request->role ?? 'client',
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user'  => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Email ou mot de passe incorrect'], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = JWTAuth::fromUser($user);
+        return response()->json([
+            'user'  => auth()->user(),
+            'token' => $token
+        ], 200);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+}
