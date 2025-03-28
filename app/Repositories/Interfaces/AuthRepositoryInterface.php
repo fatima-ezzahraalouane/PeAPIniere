d440a1bb412c5be8b30ce11974b5cebdeb84e241
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-28 12:23:24 +0000

    PEAP-28 Refactor AuthRepositoryInterface to use DTOs for register and login methods

--- app/Repositories/Interfaces/AuthRepositoryInterface.php
+++ app/Repositories/Interfaces/AuthRepositoryInterface.php
@@ -4,9 +4,12 @@ namespace App\Repositories\Interfaces;
 
 use Illuminate\Http\Request;
 
+use App\DTO\RegisterDTO;
+use App\DTO\LoginDTO;
+
 interface AuthRepositoryInterface
 {
-    public function register(Request $request);
-    public function login(Request $request);
+    public function register(RegisterDTO $data);
+    public function login(LoginDTO $data);
     public function me();
 }
